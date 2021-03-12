8fbd0e9b31f1b6e58142c66d81c467c1ded7c74f
aninwan <sasuketeme14@gmail.com>
2021-03-12 08:28:48 +0700

    removeDebug

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
